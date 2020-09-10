c93899aefcdfd129e3bd608f5490d1723b239441
d-sawada <d_sawada@ga-tech.co.jp>
2020-09-10 21:43:58 +0900

    intro formatter

--- components/HTable.vue
+++ components/HTable.vue
@@ -1,12 +1,13 @@
 <template>
-  <hot-table
-    :data="data"
-    :settings="tableSettings"
-  />
+  <hot-table :settings="tableSettings" />
 </template>
 
 <script>
+import 'handsontable/languages/ja-JP.js'
+import moment from 'moment'
 import { HotTable } from '@handsontable/vue'
+
+moment.locale('ja')
 
 export default {
   components: {
@@ -15,7 +16,11 @@ export default {
 
   props: {
     data: {
-      type: Object,
+      type: Array,
+      required: true
+    },
+    columns: {
+      type: Array,
       required: true
     },
     settings: {
@@ -25,13 +30,65 @@ export default {
   },
 
   computed: {
+    colHeaders () {
+      return this.columns.map(column => column.header)
+    },
+    columnSettings () {
+      return this.columns.map(column =>
+        Object.assign({}, this.formatters[column.type], {
+          data: column.field
+        })
+      )
+    },
     tableSettings () {
       return Object.assign({}, {
         licenseKey: 'non-commercial-and-evaluation',
-        colHeader: true,
-        manualColumnMove: true
+        language: 'ja-JP',
+        manualColumnMove: true,
+        columnSorting: true,
+        sortEmptyCells: true,
+        data: this.data,
+        colHeaders: this.colHeaders,
+        columns: this.columnSettings
       }, this.settings)
-    }
+    },
+    formatters: () => ({
+      numeric: {
+        type: 'numeric'
+      },
+      currensy: {
+        type: 'numeric',
+        numericFormat: {
+          pattern: '¥0,0',
+          culture: 'ja-JP'
+        }
+      },
+      tenthousand: {
+        type: 'numeric',
+        numericFormat: {
+          pattern: '¥0,0',
+          culture: 'ja-JP'
+        }
+      },
+      date: {
+        type: 'date',
+        dateFormat: 'YYYY/MM/DD',
+        correctFormat: true,
+        defaultDate: moment().format('YYYY/MM/DD'),
+        datePickerConfig: {
+          yearSuffix: '年',
+          showMonthAfterYear: true,
+          showDaysInNextAndPreviousMonths: true,
+          i18n: {
+            previousMonth: '前月',
+            nextMonth: '次月',
+            months: moment.localeData()._months,
+            weekdays: moment.localeData()._weekdays,
+            weekdaysShort: moment.localeData()._weekdaysShort
+          }
+        }
+      }
+    })
   }
 }
 </script>
--- pages/handson_table.vue
+++ pages/handson_table.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h-table :data="data" :settings="settings" />
+    <h-table :data="data" :columns="columns" />
   </div>
 </template>
 
@@ -14,15 +14,35 @@ export default {
 
   data: () => ({
     data: [
-      ['', 'Ford', 'Volvo', 'Toyota', 'Honda'],
-      ['2016', 10, 11, 12, 13],
-      ['2017', 20, 11, 14, 13],
-      ['2018', 30, 15, 12, 13]
+      {
+        settlementDate: '2020/08/10',
+        loanMoney: 50000,
+        otherFee: 37800,
+        customerName: '澤田 大輔',
+        customerAge: 24
+      },
+      {
+        settlementDate: '2020/08/20',
+        loanMoney: 40000,
+        otherFee: 37800,
+        customerName: '澤田 大輔',
+        customerAge: 24
+      },
+      {
+        settlementDate: '2020/08/30',
+        loanMoney: 30000,
+        otherFee: 37800,
+        customerName: '澤田 大輔',
+        customerAge: 24
+      }
+    ],
+    columns: [
+      { header: '取引日', field: 'settlementDate', type: 'date' },
+      { header: '融資金額(千円)', field: 'loanMoney', type: 'tenthousand' },
+      { header: 'その他費用', field: 'otherFee', type: 'currensy' },
+      { header: '顧客氏名', field: 'customerName', type: 'text' },
+      { header: '年齢', field: 'customerAge', type: 'numeric' }
     ]
-  }),
-
-  computed: {
-    settings: () => ({})
-  }
+  })
 }
 </script>
