5c8d9538b9b5446901acb558318af7e4788d3497
d-sawada <d_sawada@ga-tech.co.jp>
2020-09-10 13:33:23 +0900

    ag-grid-vue

---
+++ components/AgGrid.vue
@@ -0,0 +1,84 @@
+<template>
+  <ag-grid-vue
+    ref="grid"
+    class="my-ag-grid"
+    :class="agGridClass"
+    :default-col-def="defaultColDef"
+    :column-defs="columnDefs"
+    :row-data="rowData"
+  />
+</template>
+
+<script>
+import '@/node_modules/ag-grid-community/dist/styles/ag-grid.css'
+import '@/node_modules/ag-grid-community/dist/styles/ag-theme-alpine-dark.css'
+import '@/node_modules/ag-grid-community/dist/styles/ag-theme-alpine.css'
+import '@/node_modules/ag-grid-community/dist/styles/ag-theme-balham-dark.css'
+import '@/node_modules/ag-grid-community/dist/styles/ag-theme-balham.css'
+import '@/node_modules/ag-grid-community/dist/styles/ag-theme-blue.css'
+import '@/node_modules/ag-grid-community/dist/styles/ag-theme-bootstrap.css'
+import '@/node_modules/ag-grid-community/dist/styles/ag-theme-dark.css'
+import '@/node_modules/ag-grid-community/dist/styles/ag-theme-fresh.css'
+import '@/node_modules/ag-grid-community/dist/styles/ag-theme-material.css'
+import { AgGridVue } from 'ag-grid-vue'
+
+export default {
+  components: {
+    AgGridVue
+  },
+
+  props: {
+    theme: {
+      type: String,
+      required: true
+    }
+  },
+
+  computed: {
+    defaultColDef: () => ({
+      filter: true,
+      sortable: true,
+      editable: true,
+      resizable: true
+    }),
+    columnDefs: () => [
+      { headerName: 'Make', field: 'make' },
+      { headerName: 'Model', field: 'model' },
+      { headerName: 'Price', field: 'price' }
+    ],
+    rowData: () => [
+      { make: 'Toyota', model: 'Celica', price: 35000 },
+      { make: 'Ford', model: 'Mondeo', price: 32000 },
+      { make: 'Porsche', model: 'Boxter', price: 72000 }
+    ],
+    agGridClass () {
+      return `ag-theme-${this.theme}`
+    }
+  },
+
+  mounted () {
+    this.sizeColumnsToFit()
+  },
+
+  methods: {
+    sizeColumnsToFit () {
+      this.$refs.grid.gridOptions.api.sizeColumnsToFit()
+    },
+    autoSizeAll () {
+      const allColumnIds = []
+      this.$refs.grid.gridOptions.columnApi.getAllColumns().forEach((column) => {
+        allColumnIds.push(column.colId)
+      })
+      this.$refs.grid.gridOptions.columnApi.autoSizeColumns(allColumnIds)
+      this.sizeColumnsToFit()
+    }
+  }
+}
+</script>
+
+<style>
+.my-ag-grid {
+  width: 100%;
+  height: 500px;
+}
+</style>
